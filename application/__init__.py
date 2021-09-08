8ce5e4fc347cc15b6a3e660f443fc919df94b362
Josh <joshmessitte@gmail.com>
2021-09-08 10:45:34 -0400

    Spelling Fix in init.py

--- application/__init__.py
+++ application/__init__.py
@@ -27,12 +27,12 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
 
     from .models import User
-    create_databae(app)
+    create_database(app)
 
     return app
 
 
-def create_databae(app):
+def create_database(app):
     if not path.exists("application/" + DB_NAME):
         db.create_all(app=app)
         print("Created Database!")

e1ceefdf8d086cc230fed2ab92d5e5156b052990
Genki Aikawa <ga07600@uga.edu>
2021-09-25 18:21:34 -0400

    Added additional model field and created logic to read csv file and put it in the models data

--- application/models.py
+++ application/models.py
@@ -10,3 +10,14 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
+
+class Prisoner(db.Model):
+    arresting_agency = db.Column(db.String(length=6), nullable=False, unique=False)
+    grade_of_charge = db.Column(db.String(length=1), nullable=False, unique=True)
+    charge_description = db.Column(db.String(length=1024), nullable=False, unique=False)
+    bond_amount = db.Column(db.Float(), nullable=False, unique=False)
+    disqualifies = db.Column(db.Boolean(), nullable=False, unique=False)
+    mid = db.Column(db.Integer(), primary_key=True, nullable=False, unique=True)
+    name = db.Column(db.String(), nullable=False, unique=False)
+    url = db.Column(db.String(), nullable=False, unique=True)
+    qualifies = db.Column(db.Boolean(), nullable=False, unique=False)
--- application/views.py
+++ application/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
-from .models import User
+import csv
+from flask import Blueprint, render_template, redirect, url_for
+from .models import User, Prisoner
+from application import db
 
 views = Blueprint('views', __name__)
 
@@ -8,3 +10,41 @@ views = Blueprint('views', __name__)
 def home():
     users = User.query.all()
     return render_template("home.html", users=users)
+
+@views.route('/prisoners')
+def prisoners():
+    prisoners = Prisoner.query.all()
+    return render_template("prisoners.html", prisoners=prisoners)
+
+@views.route('/load')
+def load_data():
+    with open("aadm_db.csv") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+
+        try:
+            for row in csv_reader:
+                print("Adding Prisoner " + str(line_count))
+                if line_count == 0:
+                    line_count += 1
+                else:
+                    line_count += 1
+                    prisoner = Prisoner(
+                        arresting_agency=row[1],
+                        grade_of_charge=row[2],
+                        charge_description=row[3],
+                        bond_amount=float(row[4]),
+                        disqualifies=(row[5] == 'True'),
+                        mid=int(row[6]),
+                        name=row[7],
+                        url=row[8],
+                        qualifies=(row[9] != '0')
+                    )
+                    db.session.add(prisoner)
+                    db.session.commit()
+
+            return redirect(url_for("/"))
+        except Exception as e:
+            db.session.rollback()
+
+    return "<h1>Uploading CSV!</h1>"
